971ebfea6c2bca28356a2e40a0801b257dcbc3e6
Ivan <pokarayano@gmail.com>
2019-04-10 14:20:36 +0300

    set up config

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { postgraphile } = require("postgraphile");
+const graphqlHTTP = require('express-graphql');
+const cors=require('cors')
+require('dotenv').config()
+
+const app = express();
+
+app.use(postgraphile(process.env.DATABASE_URL,null, {
+    graphiql: true,
+    enableCors: true
+}));
+
+app.use(cors())
+
+app.use('/graphql', graphqlHTTP({
+  graphiql: true
+}));
+ 
+app.listen(process.env.PORT );
