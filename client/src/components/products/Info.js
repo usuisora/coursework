3c20b865a97ab4811105e1a06024c7b2e4ff534d
Ivan <pokarayano@gmail.com>
2019-04-26 13:15:24 +0300

    info

--- client/src/components/products/Info.js
+++ client/src/components/products/Info.js
@@ -1,31 +1,50 @@
 import React from 'react'
 import {MyContext} from '../../Provider'
 
-function Info() {
-  return (
-    <MyContext.Consumer>
-      {({productInfo})=>(
-        <div className = 'info'>
-          <h4>Info</h4>
-          <ul>
-              <li>id: <span>{(productInfo.prodId == undefined) ? ' ...':productInfo.prodId }</span></li>
-              <li>mark : <span>{(productInfo.prodId == undefined) ? ' ...':productInfo.mark }</span></li>
-              <li>color: <span>{(productInfo.prodId == undefined) ? ' ...':productInfo.color }</span></li>
-              <li>price: <span>{(productInfo.prodId == undefined) ? ' ...': productInfo.price }</span></li>
-          </ul>
+const handleBuy=()=>{
+  console.log('handle buy')
+}
+const Buy = () =>
+<form>
+  <input type='number' placeholder = 'count...'/>
+  <button onClick={()=>handleBuy()}>Buy</button>
+</form>
 
-           <p>There is {productInfo.avalCount} in shop point # {productInfo.shopId}</p>
-          <form>
-              <input type='number' placeholder = 'count...'/>
-              <button>Buy</button>
-          </form>
-          <form>
+const Remove =() =>
+         <form>
               <input type='number' placeholder = 'remove...'/>
               <button>Remove</button>
           </form>
-      
-    </div>
-      )}
+
+const ProductActions = () =>
+<div className="actions">
+  <Buy/>
+  <Remove/>
+</div>
+
+const InfoList = ({productInfo})=>
+(productInfo.prodId === undefined) ? (<p></p>):(
+<ul>
+  <li>id: <span>{productInfo.prodId }</span></li>
+  <li>mark : <span>{productInfo.mark }</span></li>
+  <li>color: <span>{productInfo.color }</span></li>
+  <li>price: <span>{ productInfo.price }</span></li>
+</ul>
+)
+
+function Info() {
+  return (
+    <MyContext.Consumer>
+      {({productInfo})=>
+        <div className = 'info'>
+          <h4>Info</h4>
+          <InfoList productInfo={productInfo}/>
+         { productInfo.prodId === undefined ? 
+                      (<p></p>) :
+                       (<p>There is {productInfo.avalCount} in shop point # {productInfo.shopId}</p>)}
+          <ProductActions/>
+        </div>
+          }
     </MyContext.Consumer>
   )
 }
